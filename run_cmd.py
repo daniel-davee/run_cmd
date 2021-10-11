124e51ffd11ac336f406ba695736996c174af230
daniel-davee <daniel.v.davee@gmail.com>
2021-10-11 12:56:43 -0700

    fixed __init__.py

--- run_cmd.py
+++ run_cmd.py
@@ -1,5 +1,8 @@
 from subprocess import Popen, PIPE as l
 def run_cmd(cmd:str, split=True):
+    '''
+    run_cmd in python with out Popen
+    '''
     out, err = Popen(cmd,shell=True,stdout=l).communicate()
     if err: raise OSError(err)
     return out.decode().split('\n') if split else out.decode()
