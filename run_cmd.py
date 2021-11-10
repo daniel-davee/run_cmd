ef6db69b08321c06ca0f567c5271fd76fd23ebb7
daniel-davee <daniel.v.davee@gmail.com>
2021-11-09 19:56:07 -0700

    added simple logger

--- run_cmd.py
+++ run_cmd.py
@@ -1,8 +1,28 @@
 from subprocess import Popen, PIPE as l
-def run_cmd(cmd:str, split=True):
+# import logging
+
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+from pysimplelog import Logger
+
+logger = Logger(__name__)
+logger.set_log_file_basename('run_cmd')
+logger.set_minimum_level(0)
+
+
+def run_cmd(cmd:str, split=False):
+    
     '''
     run_cmd in python with out Popen
     '''
+    
+    cmd_msg = f"""0001###
+                  {cmd=}{type(cmd)=}
+                  {split=}{type(split)=}"""
+    logger.debug(cmd_msg)
     out, err = Popen(cmd,shell=True,stdout=l).communicate()
-    if err: raise OSError(err)
+    logger.debug(out)
+    if err:
+        logger.error(err, stack_info= True)
+        raise OSError(err)
     return out.decode().split('\n') if split else out.decode()
