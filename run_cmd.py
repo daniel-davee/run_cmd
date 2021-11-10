6484200afa1d94796847a49957548338d170a3de
daniel-davee <daniel.v.davee@gmail.com>
2021-11-10 00:14:04 -0700

    updated logging framwork

--- run_cmd.py
+++ run_cmd.py
@@ -4,10 +4,11 @@ from subprocess import Popen, PIPE as l
 # logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 from pysimplelog import Logger
+from inspect import getframeinfo, currentframe
 
 logger = Logger(__name__)
 logger.set_log_file_basename('run_cmd')
-logger.set_minimum_level(0)
+logger.set_minimum_level(logger.logLevels['debug'])
 
 
 def run_cmd(cmd:str, split=False):
@@ -16,13 +17,21 @@ def run_cmd(cmd:str, split=False):
     run_cmd in python with out Popen
     '''
     
-    cmd_msg = f"""0001###
+    debug_msg = f"""########
+                  {getframeinfo(currentframe())=}
                   {cmd=}{type(cmd)=}
                   {split=}{type(split)=}"""
-    logger.debug(cmd_msg)
+    logger.debug(debug_msg)
+    
     out, err = Popen(cmd,shell=True,stdout=l).communicate()
-    logger.debug(out)
+    debug_msg = f"""What is {out=}?
+                    What is {err=}?"""
+    logger.debug(debug_msg)
+    
     if err:
-        logger.error(err, stack_info= True)
+        error_msg = f"""There was an error:
+                        {err}
+                        """
+        logger.error(error_msg, stack_info= True)
         raise OSError(err)
     return out.decode().split('\n') if split else out.decode()
